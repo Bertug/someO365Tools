047d19057fcb787946d76c92301b529ee6f57f1d
Bertug <bertugyemen@outlook.com>
2020-12-18 00:52:16 +0300

    Add files via upload
    Provide json of accepted domains, check MX record and out the result to txt

---
+++ MXcheck from Accepted Domain Json.ps1
@@ -0,0 +1,9 @@
+﻿$file = Get-Content -Path AcceptedDomains.json | ConvertFrom-Json
+
+$acceptedDomains = $file | select name
+
+forEach($x in $acceptedDomains)
+{
+    Resolve-DnsName -name $x.name -Type MX >> Coutput.txt
+}
+

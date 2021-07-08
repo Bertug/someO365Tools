792dbd8efaaee5685685fcf13c323e830f531686
Bertug <bertugyemen@outlook.com>
2021-07-08 12:56:33 +0300

    Add files via upload
    Make all public teams private script

---
+++ Make all public teams private.ps1
@@ -0,0 +1,8 @@
+﻿# Install-Module -Name MicrosoftTeams -Force -AllowClobber
+# Import-Module MicrosoftTeams
+
+$credential = Get-Credential
+Connect-MicrosoftTeams -Credential $credential
+
+$teams = Get-Team -Visibility Public
+foreach($team in $teams){Set-Team -GroupId $team.GroupId -Visibility Private}

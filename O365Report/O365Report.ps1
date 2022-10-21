b10319aa83ba43230bafaf55bb53111b09cca2b1
Bertug <bertugyemen@outlook.com>
2022-10-21 16:09:29 +0200

    Update O365Report.ps1

--- O365Report/O365Report.ps1
+++ O365Report/O365Report.ps1
@@ -9,4 +9,4 @@ $Licenses = Get-AzureADSubscribedSku | Select-Object -Property SkuPartNumber, Co
 $Users = Get-AzureADUser | Where-Object {$_.UserType -eq "Member"} | Select-Object -Property UserPrincipalName, DisplayName, Country, Department | ConvertTo-Html -Fragment -PreContent "<h1>Office 365 Users</h1>"
 $Guest = Get-AzureADUser | Where-Object {$_.UserType -eq "Guest"} | Select-Object -Property UserPrincipalName, Mail, DisplayName | ConvertTo-Html -Fragment -PreContent "<h1>Office 365 Guest Users</h1>"
 
-ConvertTo-Html -Body "$Header $Licenses $Users $Guest"  -CssUri "C:\Users\beyemen\Documents\Scripts\styles.css"  -Title "O365 Report"  | Out-File -encoding ASCII Licenses3.html
+ConvertTo-Html -Body "$Header $Licenses $Users $Guest"  -CssUri "styles.css"  -Title "O365 Report"  | Out-File -encoding ASCII Licenses3.html
